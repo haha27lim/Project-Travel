266dfdeedf022d511f51addcab18b53e4e5c0d50
haha27lim <eileen2708@hotmail.com>
2025-01-15 00:27:41 +0800

    adjust the vite.config.ts for PWA, addings those images for icons, remove manifest.webmanifest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,10 +18,48 @@ export default defineConfig(({ mode }) => {
           theme_color: '#ffffff',
           icons: [
             {
+              "src": "/icon-48x48.png",
+              "sizes": "48x48",
+              "type": "image/png"
+            },
+            {
+              "src": "/icon-72x72.png",
+              "sizes": "72x72",
+              "type": "image/png"
+            },
+            {
+              "src": "/icon-96x96.png",
+              "sizes": "96x96",
+              "type": "image/png"
+            },
+            {
+              "src": "/icon-144x144.png",
+              "sizes": "144x144",
+              "type": "image/png"
+            },
+            {
+              src: '/icon-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: '/icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            },
+            {
               src: '/icon.svg',
-              sizes: '192x192 512x512',
+              sizes: 'any',
               type: 'image/svg+xml',
-              purpose: 'any maskable'
+              purpose: 'maskable'
+            }
+          ],
+          screenshots: [
+            {
+              src: '/screenshot-desktop.png',
+              sizes: '1920x1080',
+              type: 'image/png',
+              form_factor: 'wide'
             }
           ]
         }
